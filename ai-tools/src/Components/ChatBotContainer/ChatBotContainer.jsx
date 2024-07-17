a394aa0656c9484b0886484f05e01872bb19ba9e
Kaustubh <salunkhekaustubh2004@gmail.com>
2024-07-17 20:40:53 +0530

    Working on ChatBot AI

--- ai-tools/src/Components/ChatBotContainer/ChatBotContainer.jsx
+++ ai-tools/src/Components/ChatBotContainer/ChatBotContainer.jsx
@@ -1,38 +1,104 @@
-import React from "react";
-import styles from './ChatBotContainer.module.css';
+import React, { useState } from "react";
+// import styles from './ChatBotContainer.module.css';
+import ChatBotHeader from "./ChatBotHeader/ChatBotHeader";
+import ChatBotMessageContainer from "./ChatBotMessageContainer/ChatBotMessageContainer";
+import ChatBotFooter from "./ChatBotFooter/ChatBotFooter";
+
+const questionDomain = [
+    'Who are you?',
+    'What is this Course?',
+    'What is AI?'
+];
+
+const answerDomain = [
+    "I am AI based Support System",
+    "This is Udemy course",
+    "AI stands for Artificial Intelligence"
+];
+
+const prompt = (message) =>
+    `if the question is related to ${questionDomain.join(
+        ', '
+    )} - answer it from: ${answerDomain.join(
+        ', '
+    )} where question is ${message}, else say: Please try some other question`;
 
 export default function ChatBotContainer() {
+
+    const [messages, setMessages] = useState([
+        {
+            message: 'Hi, how may I help you?',
+            userType: 'assistant',
+            createdAt: new Date()
+        },
+        {
+            message: 'E.g. *What is this course about*?',
+            userType: 'assistant',
+            createdAt: new Date()
+        }
+    ]);
+    const [message, setMessage] = useState('');
+
+    const onInputChangeHandler = (event) => {
+        event.preventDefault();
+
+        const message = event.target.value;
+        setMessage(message);
+    };
+
+    const onSubmitChangeHandler = () => {
+        const newMessage = {
+            userType: 'user',
+            message: message.trim(),
+            createdAt: new Date()
+        };
+
+        let updatedMessages = [...messages, newMessage];
+        setMessage(updatedMessages);
+        setMessage('');
+
+        // TODO Call API for ChatBot
+        fetch('/api/chatbot', {
+            method: 'POST',
+            body: JSON.stringify({
+                message: {
+                    role: newMessage.userType,
+                    content: prompt(newMessage.message)
+                }
+            })
+        })
+            .then(async (response) => {
+                const responseData = await response.json();
+                const newMessage = {
+                    message: responseData.data.content,
+                    userType: responseData.data.role,
+                    createdAt: new Date()
+                };
+
+                updatedMessages = [...updatedMessages, { ...newMessage }];
+                setMessages(updatedMessages);
+            })
+            .catch((error) => {
+                console.log('Error', error);
+            });
+    };
+
     return (
         <div className="row justify-content-center">
             <div className="col-6">
-                <div className="rounded-top">
-                    <div className="p-3 rounded-top bg-dark">
-                        <p className="mb-0 text-white">My AI</p>
-                    </div>
-                </div>
 
-                <div className={`${styles.MessageBoxContainer} px-3 py-4`}>
-                    <div className={`${styles.SenderMessage} d-flex align-items-center`}>
-                        <p className={`${styles.MessageText} mb-0`}>KAKKAKAKAKAKAKKAKA</p>
-                    </div>
+                <ChatBotHeader />
 
-                    <div className={`${styles.ReceiverMessage} d-flex align-items-center justify-content-end mt-3`}>
-                        <p className={`${styles.MessageText} mb-0`}>KAKKAKAKAKAKAKKAKA</p>
-                    </div>
-                </div>
+                <ChatBotMessageContainer
+                    messages={messages}
+                />
 
-                <div className="input-group mb-3 py-1">
-                    <input className="form-control rounded-0" type="text" placeholder="Type a message..." />
-                    <button className="bg-white border-0">
-                        <img
-                            class="me-1"
-                            src="https://cdn-icons-png.flaticon.com/512/1933/1933005.png"
-                            height='20px'
-                            width='20px'
-                            alt=""
-                        />
-                    </button>
-                </div>
+                <ChatBotFooter
+                    message={message}
+                    onInputChangeHandler={onInputChangeHandler}
+                    onSubmitChangeHandler={onSubmitChangeHandler}
+                />
+
             </div>
         </div>
     );
